62b5d1fd68964ae00a28a06086e08fe349ca1404
Gqia613 <59592211+Gqia613@users.noreply.github.com>
2020-09-20 18:09:51 +0900

    Update db.php

--- portfolio/db.php
+++ portfolio/db.php
@@ -1,10 +1,7 @@
 <?php
 
 $conn = "dbname=dbc3vqnkgkk489 host=ec2-52-1-95-247.compute-1.amazonaws.com port=5432 user=jwvivlcaqucweh password=8bafc8441900fc7cdff15349aa62d905003ccfe189fa02f9fdcfaa67fbb589f3 sslmode=require";
-$pdo = pg_connect($conn);
+$pdo = new PDO($conn,[
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+]);
 
-if (!$pdo) {
-    die('接続失敗です。'.pg_last_error());
-}
-
-print('接続に成功しました。<br>');
