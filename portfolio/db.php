209bb736bb24b743df22eeb38a7dca20a5652a27
Gqia613 <59592211+Gqia613@users.noreply.github.com>
2020-09-20 19:00:57 +0900

    Update db.php

--- portfolio/db.php
+++ portfolio/db.php
@@ -1,10 +1,18 @@
 <?php
-$DBHOST = "ec2-52-1-95-247.compute-1.amazonaws.com";
-$DBPORT = "5432";
-$DBNAME = "dbc3vqnkgkk489";
-$DBUSER = "jwvivlcaqucweh";
-$DBPASS = "8bafc8441900fc7cdff15349aa62d905003ccfe189fa02f9fdcfaa67fbb589f3";
+// $DBHOST = "ec2-52-1-95-247.compute-1.amazonaws.com";
+// $DBPORT = "5432";
+// $DBNAME = "dbc3vqnkgkk489";
+// $DBUSER = "jwvivlcaqucweh";
+// $DBPASS = "8bafc8441900fc7cdff15349aa62d905003ccfe189fa02f9fdcfaa67fbb589f3";
 
-$dbh = new PDO("pgsql:host=$DBHOST;port=$DBPORT;dbname=$DBNAME;user=$DBUSER;password=$DBPASS");
-print("接続成功".'<br>');
+// $dbh = new PDO("pgsql:host=$DBHOST;port=$DBPORT;dbname=$DBNAME;user=$DBUSER;password=$DBPASS");
+// print("接続成功".'<br>');
 
+
+$dsn = 'pgsql:host=ec2-52-1-95-247.compute-1.amazonaws.com;port=5432;dbname=dbc3vqnkgkk489;charset=utf8';
+$user = 'jwvivlcaqucweh';
+$pass = '8bafc8441900fc7cdff15349aa62d905003ccfe189fa02f9fdcfaa67fbb589f3';
+
+$pdo = new PDO($dsn,$user,$pass,[
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+]);
