42198613a2aead5fd07068e8a1814e09c4830039
Gqia613 <59592211+Gqia613@users.noreply.github.com>
2020-09-20 17:44:37 +0900

    Create db.php

---
+++ portfolio/db.php
@@ -0,0 +1,4 @@
+<?php
+
+$conn = "host=ec2-52-1-95-247.compute-1.amazonaws.com dbname=dbc3vqnkgkk489 user=jwvivlcaqucweh password=8bafc8441900fc7cdff15349aa62d905003ccfe189fa02f9fdcfaa67fbb589f3";
+$pdo = pg_connect($conn);
