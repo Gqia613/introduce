4f4e282f584750f3686c43f2c13c5385bcea0c4d
Gqia613 <59592211+Gqia613@users.noreply.github.com>
2020-09-20 17:59:34 +0900

    Update db.php

--- portfolio/db.php
+++ portfolio/db.php
@@ -1,4 +1,4 @@
 <?php
 
-$conn = "host=ec2-52-1-95-247.compute-1.amazonaws.com dbname=dbc3vqnkgkk489 user=jwvivlcaqucweh password=8bafc8441900fc7cdff15349aa62d905003ccfe189fa02f9fdcfaa67fbb589f3";
+$conn = "dbname=dbc3vqnkgkk489 host=ec2-52-1-95-247.compute-1.amazonaws.com port=5432 user=jwvivlcaqucweh password=8bafc8441900fc7cdff15349aa62d905003ccfe189fa02f9fdcfaa67fbb589f3 sslmode=require";
 $pdo = pg_connect($conn);
